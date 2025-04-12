0809a28c6718fe20ebbaf98add3069c2b5374244
Olsney <104034829+Olsney@users.noreply.github.com>
2025-04-13 01:19:03 +0300

    Updated const in standalone input service

--- Assets/CodeBase/Services/InputServices/StandaloneInputService.cs
+++ Assets/CodeBase/Services/InputServices/StandaloneInputService.cs
@@ -6,8 +6,8 @@ namespace CodeBase.Services.InputServices
     public class StandaloneInputService : IInputService
     {
         private const string Horizontal = nameof(Horizontal);
+        private const string Vertical = nameof(Vertical);
         private const KeyCode Jump = KeyCode.Space;
-        private const string Vertical = "Vertical";
 
         public Vector3 Direction => new Vector3(Input.GetAxis(Horizontal), 0, Input.GetAxis(Vertical));
         public bool IsJumping => Input.GetKeyDown(Jump);
